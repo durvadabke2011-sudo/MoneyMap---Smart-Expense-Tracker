1ba1a06216bef591fe81fc4eaea09b8c07091161
Durvashri Dabke <durvashri.dabke@gmail.com>
2026-02-26 11:16:21 +0530

    fix db connection

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-DB_HOST     = os.environ.get("DB_HOST", "localhost")
-DB_USER     = os.environ.get("DB_USER", "moneymap_user")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "moneymap123")
-DB_NAME     = os.environ.get("DB_NAME", "moneymap")
+DB_HOST     = os.environ.get("DB_HOST", "switchyard.proxy.rlwy.net")
+DB_PORT     = int(os.environ.get("DB_PORT", 39650))
+DB_USER     = os.environ.get("DB_USER", "root")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "SJcAmhIgmnJUJCEJpOMuskAUQDULGUic")
+DB_NAME     = os.environ.get("DB_NAME", "railway")
 
 SECRET_KEY  = os.environ.get("SECRET_KEY", "moneymap_super_secret_2024")
